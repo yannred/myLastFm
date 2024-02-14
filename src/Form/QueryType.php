9a5d34f3d32c2c48a39c4af75cf54e7f7445a975
Yann <mail@it.yann.red>
2024-02-14 12:30:47 +0100

    wip

--- src/Form/QueryType.php
+++ src/Form/QueryType.php
@@ -2,10 +2,12 @@
 
 namespace App\Form;
 
+use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class QueryType extends AbstractType
 {
@@ -17,4 +19,11 @@ class QueryType extends AbstractType
             ->add('to', DateType::class)
         ;
     }
+
+  public function configureOptions(OptionsResolver $resolver): void
+  {
+    $resolver->setDefaults([
+      'data_class' => User::class,
+    ]);
+  }
 }
