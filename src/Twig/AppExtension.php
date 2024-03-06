55ce6c9c1803b41e2f6be9e78017e558cc319320
Yann <mail@it.yann.red>
2024-03-06 01:41:21 +0100

    update My Album page Style

--- src/Twig/AppExtension.php
+++ src/Twig/AppExtension.php
@@ -11,9 +11,16 @@ class AppExtension extends AbstractExtension
   {
     return [
       new TwigFilter('youtubeSearchLink', [$this, 'createYoutubeSearchLink']),
+      new TwigFilter('truncateAndSuspension', [$this, 'truncateAndSuspension']),
     ];
   }
 
+  /**
+   * Create a youtube search link
+   * @param string $track
+   * @param string $artist
+   * @return string
+   */
   public function createYoutubeSearchLink(string $track, string $artist): string
   {
     $artist = urlencode($artist);
@@ -23,5 +30,21 @@ class AppExtension extends AbstractExtension
     $link = 'https://www.youtube.com/results?search_query=' . $search;
 
     return $link;
+  }
+
+  /**
+   * Truncate a string and add suspension points if it's too long
+   * @param string $string
+   * @param int $lenght
+   * @return string
+   */
+  public function truncateAndSuspension(string $string, int $lenght): string
+  {
+    if (strlen($string) > $lenght) {
+      $string = substr($string, 0, $lenght);
+      $string = $string . '...';
+    }
+
+    return $string;
   }
 }
